cda2d245ef35f424460ae35505f5e5bb956c71f5
adrianromero99 <adrianromero99@htomail.com>
2020-11-02 15:17:25 -0300

    Actualizacion pruebas 4 y 5

--- TP0/pruebas/pruebas.sh
+++ TP0/pruebas/pruebas.sh
@@ -3,17 +3,22 @@ espacios() {
     echo
     echo
 }
+newline=$'\n'
+
 echo Comienza la ejecucion de las pruebas
 
 espacios
 
 	echo -e "\e[1m PRUEBA 1 \e[0m" - Se codifica un archivo vacio llamado "archivoVacio.txt"
 	echo
-	echo RESULTADO ESPERADO:
+
+	echo RESULTADO ESPERADO: 
+	resultadoEsperado="El archivo esta vacio, no hay nada para codificar."
 	echo El archivo esta vacio, no hay nada para codificar.
-	echo RESULTADO:
-	./tp -i archivoVacio.txt -o codificado.txt 2>&1
-	if ["El archivo esta vacio, no hay nada para codificar." ==  ./tp -i archivoVacio.txt -o codificado.txt]
+	echo RESULTADO OBTENIDO: 
+	resultadoObtenido=$(./tp -i archivoVacio.txt -o codificado.txt 2>&1)
+	
+	if [ resultadoEsperado == resultadoObtenido ];
 	then
 			echo -e "\e[32m PRUEBA SUPERADA \e[0m"
 	else
@@ -23,15 +28,24 @@ espacios
 
 espacios
 
-	echo -e "\e[1m PRUEBA 2 \e[0m" - Se manda el siguiente archivo que no esta en base 64 llamado "noEstoyEn64.txt":
+	echo -e "\e[1m PRUEBA 2 \e[0m" - Se manda a decodificar el archivo que no esta en base 64 llamado "noEstoyEn64.txt":
 	echo
 	echo El archivo "noEstoyEn64.txt":
 	cat noEstoyEn64.txt
 	echo
 	echo RESULTADO ESPERADO:
-	echo El archivo enviado no esta en base 64.
-	echo RESULTADO:
-	./tp -d -i noEstoyEn64.txt -o decodificado.txt 2>&1
+	resultadoEsperado=$(printf "El archivo enviado no esta en base 64.\n")
+	echo "$resultadoEsperado"
+	echo RESULTADO OBTENIDO:
+	resultadoObtenido=$(./tp -d -i noEstoyEn64.txt -o decodificado.txt 2>&1)
+	echo "$resultadoObtenido"
+	if [ "$resultadoEsperado" == "$resultadoObtenido" ];
+	then
+			echo -e "\e[32m PRUEBA SUPERADA \e[0m"
+	else
+			echo -e "\e[31m PRUEBA FALLADA \e[0m"
+	fi
+
 
 espacios
 
@@ -43,13 +57,54 @@ espacios
 	./tp -i quijote.txt -o quijoteCodificado.txt
 	echo Codificacion del archivo:
 	cat quijoteCodificado.txt
+	echo " "
+	echo RESULTADO ESPERADO:
+	resultadoEsperado="En un lugar de La Mancha de cuyo nombre no quiero acordarme"
+	echo "$resultadoEsperado"
+	./tp -d -i quijoteCodificado.txt -o quijoteDecodificado.txt
+	echo RESULTADO OBTENIDO:
+	resultadoObtenido=$(cat quijoteDecodificado.txt)
+	echo "$resultadoObtenido"
+	
+	if [ "$resultadoEsperado" == "$resultadoObtenido" ];
+	then
+			echo -e "\e[32m PRUEBA SUPERADA \e[0m"
+	else
+			echo -e "\e[31m PRUEBA FALLADA \e[0m"
+	fi
+	
+
+espacios
+
+	echo -e "\e[1m PRUEBA 4 \e[0m" - No mandamos ningun argumento al tp
+	echo " "
+	echo "Ejecutamos: ./tp"
+	echo " "
+	echo RESULTADO ESPERADO:
+	resultadoEsperado=$(printf"Debe mandar mas argumentos, o mandar por entrada estandar. Puede ver ayuda enviando el parametro -h" \n)
+	echo "$resultadoEsperado"
+	echo RESULTADO OBTENIDO:
+	resultadoObtenido="$(./tp)"
+	echo "$resultadoObtenido"	
+	if [ "$resultadoEsperado" == "$resultadoObtenido" ];
+	then
+			echo -e "\e[32m PRUEBA SUPERADA \e[0m"
+	else
+			echo -e "\e[31m PRUEBA FALLADA \e[0m"
+	fi
+	
+
+espacios
+
+	echo -e "\e[1m PRUEBA 5 \e[0m" - Mandamos un archivo inexistente por terminal
 	echo
 	echo RESULTADO ESPERADO:
-	echo En un lugar de la Mancha de cuyo nombre no quiero acordarme
-	resultadoObtenido =$(./tp -d -i quijoteCodificado.txt)
-	echo RESULTADO:
+	resultadoEsperado=$(printf "El archivo ingresado por terminal no existe o esta vacio.")
+	echo $resultadoEsperado
+	echo RESULTADO OBTENIDO:
+	resultadoObtenido=$( cat archivoInexistente.txt | ./tp 2>&1 )
 	echo $resultadoObtenido
-	if ["En un lugar de la Mancha de cuyo nombre no quiero acordarme" ==  resultadoObtenido]
+	if [ "$resultadoEsperado" == "$resultadoObtenido" ];
 	then
 			echo -e "\e[32m PRUEBA SUPERADA \e[0m"
 	else
@@ -58,37 +113,20 @@ espacios
 
 espacios
 
-	echo -e "\e[1m PRUEBA 4 \e[0m" - No mandamos ningun argumento al tp
-	echo
-	echo RESULTADO ESPERADO:
-	echo "Debe mandar mas argumentos, o mandar por entrada estandar. Puede ver ayuda enviando el parametro -h"
-	echo RESULTADO:
-	resultadoObtenido =$(./tp)
-  echo $resultadoObtenido
-  if ["Debe mandar mas argumentos, o mandar por entrada estandar. Puede ver ayuda enviando el parametro -h" ==  resultadoObtenido]
-  then
-      echo -e "\e[32m PRUEBA SUPERADA \e[0m"
-  else
-      echo -e "\e[31m PRUEBA FALLADA \e[0m"
-  fi
-
-espacios
-
-	echo -e "\e[1m PRUEBA 5 \e[0m" - Mandamos un archivo inexistente por terminal
-	echo
-	echo RESULTADO ESPERADO:
-	echo El archivo ingresado por terminal no existe o esta vacio.
-	echo RESULTADO:
-	cat archivoInexistente | ./tp 2>&1
-
-espacios
-
 	echo -e "\e[1m PRUEBA 6 \e[0m"- Al usar /dev/null no se muestran errores por stderr
 	echo RESULTADO ESPERADO:
-	echo
+	resultadoEsperado=""
+	echo $resultadoEsperado
 	echo Ejecutamos lo siguiente: ./tp 2> /dev/null
-	echo RESULTADO:
-	./tp 2> /dev/null
+	echo RESULTADO OBTENIDO:
+	resultadoObtenido=$(./tp 2> /dev/null)
+	echo $resultadoObtenido
+	if [ "$resultadoEsperado" == "$resultadoObtenido" ];
+	then
+			echo -e "\e[32m PRUEBA SUPERADA \e[0m"
+	else
+			echo -e "\e[31m PRUEBA FALLADA \e[0m"
+	fi
 
 espacios
 
