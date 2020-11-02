df6f0f283c9bd2c125c18fea5984ddb5e26b3996
Bruno Grassano <grassanobruno@gmail.com>
2020-11-02 14:04:10 -0300

    Acomodo en carptea TP0

--- TP0/pruebas/pruebas.sh
+++ TP0/pruebas/pruebas.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 espacios() {
-    echo 
-    echo
     echo
     echo
 }
@@ -11,9 +9,9 @@ espacios
 
 	echo -e "\e[1m PRUEBA 1 \e[0m" - Se codifica un archivo vacio llamado "archivoVacio.txt"
 	echo
-	echo RESULTADO ESPERADO: 
+	echo RESULTADO ESPERADO:
 	echo El archivo esta vacio, no hay nada para codificar.
-	echo RESULTADO: 
+	echo RESULTADO:
 	./tp -i archivoVacio.txt -o codificado.txt 2>&1
 	if ["El archivo esta vacio, no hay nada para codificar." ==  ./tp -i archivoVacio.txt -o codificado.txt]
 	then
@@ -31,7 +29,7 @@ espacios
 	cat noEstoyEn64.txt
 	echo
 	echo RESULTADO ESPERADO:
-	echo El archivo enviado no esta en base 64
+	echo El archivo enviado no esta en base 64.
 	echo RESULTADO:
 	./tp -d -i noEstoyEn64.txt -o decodificado.txt 2>&1
 
@@ -48,10 +46,9 @@ espacios
 	echo
 	echo RESULTADO ESPERADO:
 	echo En un lugar de la Mancha de cuyo nombre no quiero acordarme
-	resultadoObtenido = ./tp -d -i quijoteCodificado.txt -o quijoteDecodificado.txt
-	echo resultadoObtenido
+	resultadoObtenido =$(./tp -d -i quijoteCodificado.txt)
 	echo RESULTADO:
-	cat quijoteDecodificado.txt
+	echo $resultadoObtenido
 	if ["En un lugar de la Mancha de cuyo nombre no quiero acordarme" ==  resultadoObtenido]
 	then
 			echo -e "\e[32m PRUEBA SUPERADA \e[0m"
@@ -64,9 +61,16 @@ espacios
 	echo -e "\e[1m PRUEBA 4 \e[0m" - No mandamos ningun argumento al tp
 	echo
 	echo RESULTADO ESPERADO:
-	echo Faltan argumentos
+	echo "Debe mandar mas argumentos, o mandar por entrada estandar. Puede ver ayuda enviando el parametro -h"
 	echo RESULTADO:
-	./tp
+	resultadoObtenido =$(./tp)
+  echo $resultadoObtenido
+  if ["Debe mandar mas argumentos, o mandar por entrada estandar. Puede ver ayuda enviando el parametro -h" ==  resultadoObtenido]
+  then
+      echo -e "\e[32m PRUEBA SUPERADA \e[0m"
+  else
+      echo -e "\e[31m PRUEBA FALLADA \e[0m"
+  fi
 
 espacios
 
@@ -81,7 +85,7 @@ espacios
 
 	echo -e "\e[1m PRUEBA 6 \e[0m"- Al usar /dev/null no se muestran errores por stderr
 	echo RESULTADO ESPERADO:
-	echo 
+	echo
 	echo Ejecutamos lo siguiente: ./tp 2> /dev/null
 	echo RESULTADO:
 	./tp 2> /dev/null
@@ -91,9 +95,17 @@ espacios
 	echo -e "\e[1m PRUEBA 7 \e[0m" - Codificafamos un archivo con solo ceros
 	echo El archivo a codificar contiene: "'\x00\x00\x00'"
 	echo RESULTADO ESPERADO:
-	printf '\x00\x00\x00' | base64
+	esperado="$(printf '\x00\x00\x00' | base64)"
+  echo $esperado
 	echo RESULTADO:
-	printf '\x00\x00\x00' | ./tp
+	obtenido="$(printf '\x00\x00\x00' | ./tp)"
+  echo $obtenido
+  if [esperado = obtenido];
+  then
+      echo -e "\e[32m PRUEBA SUPERADA \e[0m"
+  else
+      echo -e "\e[31m PRUEBA FALLADA \e[0m"
+  fi
 
 espacios
 
@@ -101,7 +113,7 @@ espacios
 	echo Ejecutamos lo siguiente: "echo -n "" |./tp"
 	echo Y luego: "echo -n "" | ./tp 2> /dev/null"
 	echo RESULTADO ESPERADO:
-	echo 
+	echo
 	echo RESULTADO:
 	echo -n "" | ./tp 2> /dev/null
 
@@ -117,5 +129,3 @@ espacios
 	echo $?
 
 espacios
-
-
