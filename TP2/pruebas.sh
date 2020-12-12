88d8b5ceb001a688b38ac096a704f80201aab33e
adrianromero99 <adrianromero99@htomail.com>
2020-12-12 15:59:34 -0300

    Arreglamos bash
    Co-Authored-By: Joaquin Gomez <joaqogomez@gmail.com>

--- TP2/pruebas.sh
+++ TP2/pruebas.sh
@@ -11,60 +11,53 @@ espacios
 	echo -e "\e[1m PRUEBA 1 - Configuracion 1 \e[0m"  
 	echo
 
-	prueba=$(./tp -c 4 -w 4 -b 32 prueba1.mem )
-	echo $prueba
+	./tp -c 4 -w 4 -b 32 prueba1.mem
 
 	echo -e "\e[1m PRUEBA 1 - Configuracion 2 \e[0m" 
 	echo
 
-	prueba=$(./tp -c 16 -w 1 -b 128 prueba1.mem )
-	echo $prueba
-
+	./tp -c 16 -w 1 -b 128 prueba1.mem
+	
     	echo -e "\e[1m PRUEBA 2 - Configuracion 1 \e[0m"  
 	echo
 
-	prueba=$(./tp -c 4 -w 4 -b 32 prueba2.mem )
-	echo $prueba
+	./tp -c 4 -w 4 -b 32 prueba2.mem
+	
 
 	echo -e "\e[1m PRUEBA 2 - Configuracion 2 \e[0m" 
 	echo
 
-	prueba=$(./tp -c 16 -w 1 -b 128 prueba2.mem )
-	echo $prueba
-
+	./tp -c 16 -w 1 -b 128 prueba2.mem
+	
     	echo -e "\e[1m PRUEBA 3 - Configuracion 1 \e[0m"  
 	echo
 
-	prueba=$(./tp -c 4 -w 4 -b 32 prueba3.mem )
-	echo $prueba
+	./tp -c 4 -w 4 -b 32 prueba3.mem
+	
 
 	echo -e "\e[1m PRUEBA 3 - Configuracion 2 \e[0m" 
 	echo
 
-	prueba=$(./tp -c 16 -w 1 -b 128 prueba3.mem )
-	echo $prueba
-
+	./tp -c 16 -w 1 -b 128 prueba3.mem
+	echo
+	
     	echo -e "\e[1m PRUEBA 4 - Configuracion 1 \e[0m"  
 	echo
 
-	prueba=$(./tp -c 4 -w 4 -b 32 prueba4.mem )
-	echo $prueba
+	./tp -c 4 -w 4 -b 32 prueba4.mem
 
 	echo -e "\e[1m PRUEBA 4 - Configuracion 2 \e[0m" 
 	echo
 
-	prueba=$(./tp -c 16 -w 1 -b 128 prueba4.mem )
-	echo $prueba
-
+	./tp -c 16 -w 1 -b 128 prueba4.mem 
+	
    	echo -e "\e[1m PRUEBA 5 - Configuracion 1 \e[0m"  
 	echo
 
-	prueba=$(./tp -c 4 -w 4 -b 32 prueba5.mem )
-	echo $prueba
-
+	./tp -c 4 -w 4 -b 32 prueba5.mem 
+	
 	echo -e "\e[1m PRUEBA 5 - Configuracion 2 \e[0m" 
 	echo
 
-	prueba=$(./tp -c 16 -w 1 -b 128 prueba5.mem )
-	echo $prueba
-
+	./tp -c 16 -w 1 -b 128 prueba5.mem 
+	
